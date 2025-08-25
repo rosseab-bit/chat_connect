7ecf7807f85b1257c558b9f788adebb988b647e0
roseab <roseab.developer@gmail.com>
2025-08-25 12:05:38 -0300

    agrego manifest para prueba

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
+
+const withPWA = require("next-pwa")({
+  dest: "public"
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export', // esto indica que quieres un sitio estático
 };
 
 export default nextConfig;
